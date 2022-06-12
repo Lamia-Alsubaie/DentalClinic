801b0bebd6ff239e7b577b2c76654e3bf1023d51
Lamia-Alsubaie <107337114+Lamia-Alsubaie@users.noreply.github.com>
2022-06-12 17:28:27 +0300

    Add files via upload

---
+++ DentalClinic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DentalClinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
